317ce74953e1b30ed72f0b2ae70790fde6c3b333
Simon Liedl <simon.liedl@pixida.de>
2016-11-28 13:20:28 +0100

    Insert db working

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://teletubbie.test.b2b.pixida.com');
 var ProtoBuf = require("protobufjs");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var builder = ProtoBuf.loadProtoFile("tixs.proto");
 var tixsMessages = builder.build();
+
+mongoose.connect('mongodb://localhost/tixs', { config: { autoIndex: false } });
+
+var deviceSchema = new Schema({
+  imei: { type: String, index:{ unique: true}},
+  longVersion: String,
+  hwVersion: String,
+  date: { type: Date, default: Date.now }
+});
+
+
+var Device = mongoose.model('Device', deviceSchema);
 
 
 client.on('connect', function () {
@@ -13,8 +27,20 @@ client.on('connect', function () {
 });
 
 client.on('message', function (topic, message) {
-  // message is Buffer
-  msg = tixsMessages.telematik.Startup.decode(message);
+    // message is Buffer
+    msg = tixsMessages.telematik.Startup.decode(message);
+    if( msg !== null )
+    {
+        Device.update(
+            {imei: msg.getImei()},
+            {
+                $setOnInsert: {imei: msg.getImei()},
+                $set: { date: Date.now(), longVersion: msg.getLongVersion(), hwVersion: msg.getHwVersion() }
+            },
+            {upsert: true},
+            function(err,num){ console.log(num); }
+        );
+    }
   console.log(msg.getImei());
 
 });
