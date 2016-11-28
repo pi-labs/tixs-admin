0c4fdc18bcd50479a1a6dc4dd0a0dfe9beaca7ff
Simon Liedl <simon.liedl@pixida.de>
2016-11-28 11:50:06 +0100

    MQTT working

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://teletubbie.test.b2b.pixida.com');
+var client = mqtt.connect('mqtt://teletubbie.test.b2b.pixida.com');
+var ProtoBuf = require("protobufjs");
+
+var builder = ProtoBuf.loadProtoFile("tixs.proto");
+var tixsMessages = builder.build();
+
 
 client.on('connect', function () {
     console.log("Connected to Broker");
@@ -7,8 +12,9 @@ client.on('connect', function () {
     client.publish('presence', 'Hello mqtt');
 });
 
-
 client.on('message', function (topic, message) {
-  // message is Buffer 
-  console.log(message.toString());
+  // message is Buffer
+  msg = tixsMessages.telematik.Startup.decode(message);
+  console.log(msg.getImei());
+
 });
