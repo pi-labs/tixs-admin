bd490006b14bfa76abe6d656e93a01afe102dcd4
Johannes KleiN <johannes.klein@pixida.de>
2017-04-06 09:26:02 +0200

    gitignore modified to exclude keys

---
+++ config.js
@@ -0,0 +1,30 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+
+var GroupSchema = new Schema({
+    name: String,
+    config: {
+        enableTransmitAcceleration: {type: Boolean, default: true},
+        gpsFilterMinAccuracyM: {type: Number, default: 3 },
+        enableTransmitUpdatePosition: {type: Boolean, default: true},
+        enableTransmitGPS: {type: Boolean, default: true},
+        enableInactivityDetection: {type: Boolean, default: true},
+        ecallBcallSmsNumber: String,
+        secondaryEcallBcallSmsNumber: String,
+        accSendThresholdPercent: Number,
+        enableBCall: {type: Boolean, default: true},
+        enableECall: {type: Boolean, default: true},
+        inactivitySleepTimeoutSec: Number,
+        inactivityDetectionThreshold: Number,
+        agpsConfig: Number,
+        stateUpdatePeriod: Number
+    },
+    software: {
+        expected_version: {type: 'String', default: ''},
+        url: {type: 'String', default: ''}
+    },
+
+});
+
+module.exports = mongoose.model('Group', GroupSchema);
